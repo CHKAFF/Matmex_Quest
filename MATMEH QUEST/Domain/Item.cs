4cc856eb2de7db7d52e5a79f2a4061e51abdf779
CHKAFF <53139664+CHKAFF@users.noreply.github.com>
2020-04-22 14:31:16 +0500

    Create new class: Item

---
+++ MATMEH QUEST/Domain/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace MATMEH_QUEST.Domain
+{
+    public class Item
+    {
+        public int id { get; }
+        public Item(int id)
+        {
+            this.id = id;
+        }
+    }
+}
