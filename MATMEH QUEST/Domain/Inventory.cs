dd122312d76dfda4a2a3642f2e07f3a1c4af0063
CHKAFF <53139664+CHKAFF@users.noreply.github.com>
2020-04-22 14:40:10 +0500

    Add:
    Сonstructor
    Methods: PutItem, TakeItem

--- MATMEH QUEST/Domain/Inventory.cs
+++ MATMEH QUEST/Domain/Inventory.cs
@@ -1,7 +1,24 @@
-﻿namespace MATMEH_QUEST.Domain
+﻿using System.Collections.Generic;
+using MATMEH_QUEST.Domain.Item;
+
+namespace MATMEH_QUEST.Domain
 {
     public class Inventory
     {
-        
+        private HashSet<item> items;
+        public Inventory()
+        {
+            items = new HashSet<item>();
+        }
+
+        public item PutItem()
+        {
+            
+        }
+
+        public item TakeItem()
+        {
+            
+        }
     }
 }
