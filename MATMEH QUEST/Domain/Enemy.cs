fafe32befe0a7c33f4f0d6f8e56ddfaaf0c4e34f
CHKAFF <53139664+CHKAFF@users.noreply.github.com>
2020-04-22 21:17:28 +0500

    Create: Enemy.cs and use ILife

--- MATMEH QUEST/Domain/Enemy.cs
+++ MATMEH QUEST/Domain/Enemy.cs
@@ -1,7 +1,34 @@
 ﻿namespace MATMEH_QUEST.Domain
 {
-    public class Enemy
+    public class Enemy : ILife
     {
+        public int Health;
+        private PlayerState state;
         
+        public Enemy(int health)
+        {
+            this.Health = health;
+            state = PlayerState.Alive;
+        }
+        private enum PlayerState
+        {
+            Alive,
+            Dead
+        }
+
+        public bool IsAlive()
+        {
+            return Health > 0 && state == PlayerState.Alive;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+        }
+
+        public void Dead()
+        {
+            state = PlayerState.Dead;
+        }
     }
 }
