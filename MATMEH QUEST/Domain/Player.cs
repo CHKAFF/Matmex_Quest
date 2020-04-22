2456af8eb8ce4a6e21da8b6662746915096e6c8b
CHKAFF <53139664+CHKAFF@users.noreply.github.com>
2020-04-22 19:58:34 +0500

    Fix all Trash in project

--- MATMEH QUEST/Domain/Player.cs
+++ MATMEH QUEST/Domain/Player.cs
@@ -1,39 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-﻿using System.Data.Common;
 using System.Drawing;
-=======
-﻿using System.Drawing;
->>>>>>> origin/class_Player
-=======
-﻿using System.Data.Common;
-using System.Drawing;
->>>>>>> 034d769b898d3fa97d35cb6181287baf1e481e98
 
 namespace MATMEH_QUEST.Domain
 {
     public class Player
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 034d769b898d3fa97d35cb6181287baf1e481e98
-        private Point location;
-        private int health;
-        private Inventory inventory;
-
-        public Player(Point location, int health, Inventory inventory)
-        {
-            this.location = location;
-            this.health = health;
-            this.inventory = inventory;
-<<<<<<< HEAD
-=======
-        public Player()
-=======
         public Player(Point location, int health)
->>>>>>> origin/class_Player
         {
             this.Health = health;
             state = PlayerState.Alive;
@@ -44,7 +15,7 @@ namespace MATMEH_QUEST.Domain
             Alive,
             Dead
         }
-        
+
         public int Health;
         private PlayerState state;
         public Point Location;
@@ -62,9 +33,6 @@ namespace MATMEH_QUEST.Domain
         public void Dead()
         {
             state = PlayerState.Dead;
->>>>>>> class_Player
-=======
->>>>>>> 034d769b898d3fa97d35cb6181287baf1e481e98
         }
     }
 }
