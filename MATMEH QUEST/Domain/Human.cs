1e18869256e0567603fe847ed7504a618da573d0
CHKAFF <53139664+CHKAFF@users.noreply.github.com>
2020-04-22 16:15:11 +0500

    Add:
    enum {Ready, Awaiting, NotReady}
    field state
    Constructor
    Methods: IsReady, MakeReady, MakeAwaiting, MakeNotReady

--- MATMEH QUEST/Domain/Human.cs
+++ MATMEH QUEST/Domain/Human.cs
@@ -2,6 +2,38 @@ namespace MATMEH_QUEST.Domain
 {
     public class Human
     {
-        
+        public enum HumanState
+        {
+            Ready,
+            Awaiting,
+            NotReady
+        }
+
+        private HumanState state;
+
+        public Human(HumanState state) 
+        {
+            this.state = state;
+        }
+
+        public bool IsReady()
+        {
+            return state == HumanState.Ready;
+        }
+
+        public void MakeReady()
+        {
+            state = HumanState.Ready;
+        }
+
+        public void MakeNotReady()
+        {
+            state = HumanState.NotReady;
+        }
+
+        public void MakeAwaiting()
+        {
+            state = HumanState.Awaiting;
+        }
     }
 }
