034d769b898d3fa97d35cb6181287baf1e481e98
Гриша <aydartsyang@gmail.com>
2020-04-22 18:31:58 +0500

    Обновил Player

--- MATMEH QUEST/Domain/Player.cs
+++ MATMEH QUEST/Domain/Player.cs
@@ -1,7 +1,19 @@
-﻿namespace MATMEH_QUEST.Domain
+﻿using System.Data.Common;
+using System.Drawing;
+
+namespace MATMEH_QUEST.Domain
 {
     public class Player
     {
-        
+        private Point location;
+        private int health;
+        private Inventory inventory;
+
+        public Player(Point location, int health, Inventory inventory)
+        {
+            this.location = location;
+            this.health = health;
+            this.inventory = inventory;
+        }
     }
 }
