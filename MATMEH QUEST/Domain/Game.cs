c9b508dccc88128c1606883f1fb7b74b7de33455
CHKAFF <53139664+CHKAFF@users.noreply.github.com>
2020-04-23 14:21:30 +0500

    Add:
    Consrtuctor
    Field: World, Player, Inventory
    Methods: New, Load

--- MATMEH QUEST/Domain/Game.cs
+++ MATMEH QUEST/Domain/Game.cs
@@ -1,7 +1,29 @@
-﻿namespace MATMEH_QUEST.Domain
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace MATMEH_QUEST.Domain
 {
     public class Game
     {
-        
+        public World World;
+        public Player Player;
+        public Inventory Inventory;
+        public Game()
+        {
+            
+        }
+
+        public void New()
+        {
+            World = new World();
+            Player = new Player(new Point(0,0),100);
+            Inventory = new Inventory();
+        }
+
+        public void Load()
+        {
+            World = new World();
+            Player = new Player();
+        }
     }
 }
