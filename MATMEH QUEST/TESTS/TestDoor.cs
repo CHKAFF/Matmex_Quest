a2923426f02edf2206d25ce5954dbf21ea42a1ea
Гриша <aydartsyang@gmail.com>
2020-04-23 22:17:28 +0500

    Changes in TestDoor

---
+++ MATMEH QUEST/TESTS/TestDoor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MATMEH_QUEST;
+using MATMEH_QUEST.Domain;
+using NUnit.Framework;
+
+namespace TESTS
+{
+    [TestFixture]
+    public class TestDoor
+    {
+        [SetUp]
+        public Door SetUp()
+        {
+            return new Door(Door.State.Close);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var door = SetUp();
+            var excepted = new Door(Door.State.Open);
+            door.Open();
+            Assert.AreEqual(excepted, door);
+        }
+    }
+}
