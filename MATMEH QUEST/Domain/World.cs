6f0415d5adb5da5ed00c51f219a954a8b06cf808
Гриша <aydartsyang@gmail.com>
2020-04-23 16:06:53 +0500

    Changed world

--- MATMEH QUEST/Domain/World.cs
+++ MATMEH QUEST/Domain/World.cs
@@ -1,7 +1,20 @@
-﻿namespace MATMEH_QUEST.Domain
+﻿using System.Collections.Generic;
+
+namespace MATMEH_QUEST.Domain
 {
     public class World
     {
+        private readonly double height;
+        private readonly double width;
+        public List<Door> Doors{ get; set; }
+        public List<Human> Humans { get; set; }
+        public List<Enemy> Enemies { get; set; }
+
+        public World(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+        }
 
     }
 }
