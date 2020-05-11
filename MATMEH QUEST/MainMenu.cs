0da82ba8ee8e676a6c34d20efa5e70aee853e92e
CHKAFF <53139664+CHKAFF@users.noreply.github.com>
2020-05-11 16:59:41 +0500

    Я делаюГрязь

--- MATMEH QUEST/MainMenu.cs
+++ MATMEH QUEST/MainMenu.cs
@@ -42,38 +42,7 @@ namespace MATMEH_QUEST
             var graphics = e.Graphics;
             if (isMenu)
             {
-                var backgroundColor = Color.FromArgb(145, 215, 254);
-                BackColor = backgroundColor;
-
-                var logotype = new Bitmap(Resources.фон, 120, 100);
-                graphics.DrawImage(logotype, new Point(90, 0));
-
-                PrivateFontCollection fontCollection = new PrivateFontCollection();
-                fontCollection.AddFontFile("19190.TTF");
-                FontFamily family = fontCollection.Families[0];
-
-                var newGame = MakeNewButton("НОВАЯ ИГРА",
-                    new Size(100, 50),
-                    new Point(100, 100),
-                    new Font(family, 20),
-                    FlatStyle.Flat,
-                    Color.FromArgb(1, 127, 189),
-                    0,
-                    backgroundColor);
-                Controls.Add(newGame);
-
-                var exit = MakeNewButton("ВЫХОД",
-                    new Size(100, 50),
-                    new Point(100, 150),
-                    new Font(family, 20),
-                    FlatStyle.Flat,
-                    Color.FromArgb(1, 127, 189),
-                    0,
-                    backgroundColor);
-                Controls.Add(exit);
-
-                newGame.Click += NewGameOnClick;
-                exit.Click += ExitOnClick;
+                PaintMenu(graphics);
             }
             else
             {
@@ -83,8 +52,43 @@ namespace MATMEH_QUEST
                 var pictureBox = new PictureBox();
                 var background = new Bitmap(Resources.Bottom_part, 120, 100);
                 graphics.DrawImage(background, new Point(0, 0));
-                
             }
+        }
+
+        private void PaintMenu(Graphics graphics)
+        {
+            var backgroundColor = Color.FromArgb(145, 215, 254);
+            BackColor = backgroundColor;
+
+            var logotype = new Bitmap(Resources.фон, 120, 100);
+            graphics.DrawImage(logotype, new Point(90, 0));
+
+            PrivateFontCollection fontCollection = new PrivateFontCollection();
+            fontCollection.AddFontFile("19190.TTF");
+            FontFamily family = fontCollection.Families[0];
+
+            var newGame = MakeNewButton("НОВАЯ ИГРА",
+                new Size(100, 50),
+                new Point(100, 100),
+                new Font(family, 20),
+                FlatStyle.Flat,
+                Color.FromArgb(1, 127, 189),
+                0,
+                backgroundColor);
+            Controls.Add(newGame);
+
+            var exit = MakeNewButton("ВЫХОД",
+                new Size(100, 50),
+                new Point(100, 150),
+                new Font(family, 20),
+                FlatStyle.Flat,
+                Color.FromArgb(1, 127, 189),
+                0,
+                backgroundColor);
+            Controls.Add(exit);
+
+            newGame.Click += NewGameOnClick;
+            exit.Click += ExitOnClick;
         }
 
         private void ExitOnClick(object sender, EventArgs e)
